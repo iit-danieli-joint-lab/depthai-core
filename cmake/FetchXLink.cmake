e044a5d9fdc5fa0696271868df9980a22070eb56
AleMel <alessandro.melis@iit.it>
2024-06-06 12:38:49 +0200

    update fixes after @traversaro tests: - removed a flag in Flags.cmake - modified CMakeLists.txt

--- cmake/FetchXLink.cmake
+++ cmake/FetchXLink.cmake
@@ -4,14 +4,15 @@
 include(FetchContent)
 FetchContent_Declare(XLink
                      GIT_REPOSITORY https://github.com/luxonis/XLink
-                     GIT_TAG master)
+                     GIT_TAG e9eb1ef38030176ad70cddd3b545d5e6c509f1e1) # release-1.10.0)
 
 FetchContent_GetProperties(XLink)
 if(NOT XLink_POPULATED)
   message(STATUS "Fetching XLink.")
   FetchContent_Populate(XLink)
-  set(usb-1.0_DIR ${CMAKE_SOURCE_DIR}/cmake/usb-1.0)
+  #set(usb-1.0_DIR ${CMAKE_SOURCE_DIR}/cmake/usb-1.0)
   set(XLINK_LIBSUB_SYSTEM ON CACHE BOOL "" FORCE)
+  set(XLINK_ENABLE_LIBUSB ${XLINK_ENABLE_LIBUSB} CACHE BOOL "" FORCE)
   add_subdirectory(${xlink_SOURCE_DIR} ${xlink_BINARY_DIR})
 endif()
   
--- cmake/Fetchlibnop.cmake
+++ cmake/Fetchlibnop.cmake
@@ -3,8 +3,8 @@
 
 include(FetchContent)
 FetchContent_Declare(libnop
-                     GIT_REPOSITORY https://github.com/google/libnop
-                     GIT_TAG master)
+                     GIT_REPOSITORY https://github.com/luxonis/libnop
+                     GIT_TAG ab842f51dc2eb13916dc98417c2186b78320ed10)
 
 FetchContent_GetProperties(libnop)
 if(NOT libnop_POPULATED)
